d43d8d44624363ea172cc5f66891fce5874f454a
m-yahya <yahya_gis@hotmail.com>
2019-03-20 17:07:22 +0100

    app structure created

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <Header />
+    <Explorer />
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header.vue";
+import Explorer from "./components/Explorer.vue";
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Explorer
+  }
+}
+</script>
+
+<style>
+
+</style>
