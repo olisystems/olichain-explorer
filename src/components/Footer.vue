ea00c03bf16d3c99259be49385d0573b251f506e
m-yahya <yahya_gis@hotmail.com>
2019-05-03 13:39:51 +0200

    Add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="footer">&copy; OLI Systems GmbH</div>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style>
+.footer {
+  text-align: center;
+  background-color: #404040;
+  padding: 2em;
+  color: rgb(190, 185, 185);
+}
+
+.footer a {
+  display: inline-block;
+  overflow: hidden;
+  width: 24px;
+  height: 24px;
+  margin: 0 5px;
+}
+</style>
