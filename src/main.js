a08427372fdd722634af20bb33ece34343fe7756
m-yahya <yahya_gis@hotmail.com>
2019-06-11 11:53:06 +0200

    Add font awesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faFacebook, faTwitter, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faFacebook, faTwitter, faGithub)
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
 Vue.config.productionTip = false
 
 new Vue({
